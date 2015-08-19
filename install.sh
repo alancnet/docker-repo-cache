4c8727260f8172e09ef3e1574e85922d8eb7284e
Alan Colon <github@alanc.net>
2015-08-19 17:31:34 -0600

    Binary option

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+NODE=$(which node)
+if [ "$NODE" == "" ] || [ "$1" == "--bin" ] || [ "$1" == "-b" ]; then
+    INSTALL_BINARY=true
+else
+    INSTALL_BINARY=false
+fi
+
 sudo /usr/bin/docker build -t docker-repo-cache . && \
 sudo mkdir -p /opt/docker-repo-cache && \
 sudo /usr/bin/docker run \
@@ -8,4 +15,9 @@ sudo /usr/bin/docker run \
   --restart always \
   --volume /opt/docker-repo-cache:/angry-caching-proxy \
   docker-repo-cache && \
-sudo cp docker /usr/local/bin/docker
+if [ "$INSTALL_BINARY" == "true" ]; then
+    wget http://www.alanc.net/download/docker/0.0.1/docker && \
+    sudo cp docker /usr/local/bin/docker
+else
+    sudo cp docker.js /usr/local/bin/docker
+fi
